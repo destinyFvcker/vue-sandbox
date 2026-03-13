5dfcdb466791a5f39fc88def986d646140e98bd9
destinyFvcker <destinyfunker@gmail.com>
2026-03-13 16:16:08 +0800

    fix: renderer empty bug

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,6 +4,29 @@ import TableShellClient from "~/components/table/TableShellClient.vue";
 import { tanstackRenderers } from "~/components/tanstack";
 import { genColumnDefs } from "~/components/tanstack/genColumnDef";
 
+const data = [
+	{
+		name: "John Doe",
+		birthDate: "1985-06-02",
+		postalCode: "12345",
+	},
+	{
+		name: "Steve Jobs",
+		birthDate: "1955-02-24",
+		postalCode: "90210",
+	},
+	{
+		name: "Bill Gates",
+		birthDate: "1955-10-28",
+		postalCode: "90210",
+	},
+	{
+		name: "Elon Musk",
+		birthDate: "1971-06-28",
+		postalCode: "90210",
+	},
+];
+
 const schema = {
 	type: "object",
 	properties: {
@@ -13,10 +36,7 @@ const schema = {
 			description: "Please enter your name",
 			i18n: "name",
 		},
-		vegetarian: {
-			type: "boolean",
-			i18n: "vegetarian",
-		},
+
 		birthDate: {
 			type: "string",
 			format: "date",
@@ -29,43 +49,7 @@ const schema = {
 		},
 	},
 };
-
-const data = [
-	{
-		name: "John Doe",
-		vegetarian: false,
-		birthDate: "1985-06-02",
-		postalCode: "12345",
-	},
-	{
-		name: "John Doe",
-		vegetarian: false,
-		birthDate: "1985-06-02",
-		postalCode: "12345",
-	},
-	{
-		name: "John Doe",
-		vegetarian: false,
-		birthDate: "1985-06-02",
-		postalCode: "12345",
-	},
-	{
-		name: "John Doe",
-		vegetarian: false,
-		birthDate: "1985-06-02",
-		postalCode: "12345",
-	},
-	{
-		name: "John Doe",
-		vegetarian: false,
-		birthDate: "1985-06-02",
-		postalCode: "12345",
-	},
-];
-
 const columns = Object.freeze(genColumnDefs(schema, tanstackRenderers));
-
-console.log("columns:", columns);
 </script>
 
 <template>
